1940bf6ef3bac939336ec4d50771df583d25f6c9
Fernando Camillo <fernando.camillo@prima.com.br>
2022-09-01 10:59:38 -0300

    Alteração na tarefa para obter os dados do json

--- src/frontend/scripts/tarefa.js
+++ src/frontend/scripts/tarefa.js
@@ -1,52 +1,147 @@
-let dados = {
-    alunos: [
-        {
-            codigo: 1,
-            nome: "Abel Pereira Goulart",
-            situacao: "E"
-        },
-        {
-            codigo: 2,
-            nome: "Caio de Oliveira Ramos",
-            situacao: "E"
-        },
-        {
-            codigo: 3,
-            nome: "Maria Aparecida dos Santos",
-            situacao: "E"
-        }
-    ]
-}
+String.prototype.toHtmlEntities = function() {
+    return this.replace(/./gm, function(s) {
+        // return "&#" + s.charCodeAt(0) + ";";
+        return (s.match(/[a-z0-9\s]+/i)) ? s : "&#" + s.charCodeAt(0) + ";";
+    });
+};
+
+
+const Situacoes = {
+    Entregue: "E",
+    NaoEntregue: "N",
+    Incompleta: "I"
+};
+
+
+let dados = {};
+
 
 const alternar = function(elemento) {
+    let codigoAluno = $(elemento).data("codigo");
     let situacaoAtual = $(elemento).data("situacao");
     let novaSituacao = "";
-    if (situacaoAtual == "E") {
-        novaSituacao = "N";
-    } else if (situacaoAtual == "N") {
-        novaSituacao = "I";
+    if (situacaoAtual == Situacoes.Entregue) {
+        novaSituacao = Situacoes.NaoEntregue;
+    } else if (situacaoAtual == Situacoes.NaoEntregue) {
+        novaSituacao = Situacoes.Incompleta;
     } else {
-        novaSituacao = "E";
+        novaSituacao = Situacoes.Entregue;
     }
     $(elemento).data("situacao", novaSituacao);
-    aplicarHtml(elemento, novaSituacao);
+    atualizarSituacao(codigoAluno, novaSituacao);
+    aplicarHtml(elemento, situacaoAtual, novaSituacao);
 }
 
-const aplicarHtml = function(elemento, novaSituacao) {
+
+const atualizarSituacao = function(codigoAluno, novaSituacao) {
+    $.each(dados.alunos, function(i, aluno) {
+        if (aluno.codigo == codigoAluno) {
+            aluno.situacao = novaSituacao;
+            return;
+        }
+    });
+}
+
+
+const aplicarHtml = function(elemento, situacaoAtual, novaSituacao) {
     let htmlAtual = $(elemento).html();
     let htmlNovo = "";
-    if (novaSituacao == "N") {
-        htmlNovo = htmlAtual.replace("Entregou", "Não entregou").replace("fa-check", "fa-x");
-        $(elemento).removeClass("btn-success");
-        $(elemento).addClass("btn-danger");
-    } else if (novaSituacao == "I") {
-        htmlNovo = htmlAtual.replace("Não entregou", "Incompleta").replace("fa-x", "fa-triangle-exclamation");
-        $(elemento).removeClass("btn-danger");
-        $(elemento).addClass("btn-warning");
-    } else {
-        htmlNovo = htmlAtual.replace("Incompleta", "Entregou").replace("fa-triangle-exclamation", "fa-check");
-        $(elemento).removeClass("btn-warning");
-        $(elemento).addClass("btn-success");
-    }
+
+    htmlNovo = htmlAtual
+        .replace(obterDescricaoSituacao(situacaoAtual), obterDescricaoSituacao(novaSituacao))
+        .replace(obterIconeSituacao(situacaoAtual), obterIconeSituacao(novaSituacao));
+    
+    $(elemento).removeClass("btn-" + obterClasseSituacao(situacaoAtual));
+    $(elemento).addClass("btn-" + obterClasseSituacao(novaSituacao));
+
     $(elemento).html(htmlNovo);
 }
+
+
+const obterDescricaoSituacao = function(situacao) {
+    if (situacao == Situacoes.NaoEntregue) {
+        return "Não entregou";
+    } else if (situacao == Situacoes.Incompleta) {
+        return "Incompleta";
+    } else {
+        return "Entregou";
+    }
+}
+
+
+const obterClasseSituacao = function(situacao) {
+    if (situacao == Situacoes.NaoEntregue) {
+        return "danger";
+    } else if (situacao == Situacoes.Incompleta) {
+        return "warning";
+    } else {
+        return "success";
+    }
+}
+
+
+const obterIconeSituacao = function(situacao) {
+    if (situacao == Situacoes.NaoEntregue) {
+        return "fa fa-x";
+    } else if (situacao == Situacoes.Incompleta) {
+        return "fa fa-triangle-exclamation";
+    } else {
+        return "fa fa-check";
+    }
+}
+
+
+const carregarTela = function() {
+    $("#dadosGerais").html(`
+        <h2>${dados.periodo.toHtmlEntities()} - ${dados.disciplina.toHtmlEntities()} - ${dados.turma.toHtmlEntities()}</h2>
+        <h3>Aula ${dados.aula} - ${dados.dia}</h3>
+        <p><strong>Tarefa:</strong> ${dados.tarefaConteudo.toHtmlEntities()}</p>
+        <p><strong>Data para entrega:</strong> ${dados.tarefaDataEntrega}</p>
+    `);
+
+    $.each(dados.alunos, function(i, aluno) {
+        $('#listaAlunos').append(`
+            <div class="card mb-3" style="background-color: #eeeeee">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-sm-8">
+                            ${aluno.nome.toHtmlEntities()}
+                        </div>
+                        <div class="col-sm-4 text-end">
+                            <a data-codigo=${aluno.codigo} data-situacao="${aluno.situacao}" href="javascript:void(0)" onclick="alternar(this)" role="button" class="btn btn-sm btn-${obterClasseSituacao(aluno.situacao)} w-100 mt-1">
+                                <span class="${obterIconeSituacao(aluno.situacao)}" style="margin-right: 5px;"></span> ${obterDescricaoSituacao(aluno.situacao)} <span class="fa fa-caret-down" style="margin-left: 5px;"></span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `);
+    });
+}
+
+
+const carregarDados = function() {
+    //TODO: Obter dados do servidor
+    var jqxhr = $.getJSON("dadosFake/tarefas.json", function (data) {
+        dados = data;
+        carregarTela();
+        $("#botoesOperacoes").show();
+    });
+}
+
+
+const botaoSalvar = function() {
+    //TODO: Salvar no servidor
+    window.alert("Faz de conta que salvou...");
+    botaoVoltar();
+}
+
+
+const botaoVoltar = function() {
+    window.location="home.html";
+}
+
+
+$(function() {
+    carregarDados();
+});
