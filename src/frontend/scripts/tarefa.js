147c50c552b804da910e7535ae7dca2c7562e92e
Fernando Camillo <fernando.camillo@prima.com.br>
2022-09-01 09:09:04 -0300

    tarefa

--- src/frontend/scripts/tarefa.js
+++ src/frontend/scripts/tarefa.js
@@ -1,25 +1,52 @@
-let valor1 = "E";
-const alternar1 = function() {
-    if (valor1 == "E") {
-        valor1 = "N";
-        let atual = $("#entrega1").html();
-        let novo = atual.replace("Entregou", "Não entregou").replace("fa-check", "fa-x");
-        $("#entrega1").html(novo);
-        $("#entrega1").removeClass("btn-success");
-        $("#entrega1").addClass("btn-danger");
-    } else if (valor1 == "N") {
-        valor1 = "I";
-        let atual = $("#entrega1").html();
-        let novo = atual.replace("Não entregou", "Incompleta").replace("fa-x", "fa-triangle-exclamation");
-        $("#entrega1").html(novo);
-        $("#entrega1").removeClass("btn-danger");
-        $("#entrega1").addClass("btn-warning");
+let dados = {
+    alunos: [
+        {
+            codigo: 1,
+            nome: "Abel Pereira Goulart",
+            situacao: "E"
+        },
+        {
+            codigo: 2,
+            nome: "Caio de Oliveira Ramos",
+            situacao: "E"
+        },
+        {
+            codigo: 3,
+            nome: "Maria Aparecida dos Santos",
+            situacao: "E"
+        }
+    ]
+}
+
+const alternar = function(elemento) {
+    let situacaoAtual = $(elemento).data("situacao");
+    let novaSituacao = "";
+    if (situacaoAtual == "E") {
+        novaSituacao = "N";
+    } else if (situacaoAtual == "N") {
+        novaSituacao = "I";
     } else {
-        valor1 = "E";
-        let atual = $("#entrega1").html();
-        let novo = atual.replace("Incompleta", "Entregou").replace("fa-triangle-exclamation", "fa-check");
-        $("#entrega1").html(novo);
-        $("#entrega1").removeClass("btn-warning");
-        $("#entrega1").addClass("btn-success");
+        novaSituacao = "E";
     }
+    $(elemento).data("situacao", novaSituacao);
+    aplicarHtml(elemento, novaSituacao);
+}
+
+const aplicarHtml = function(elemento, novaSituacao) {
+    let htmlAtual = $(elemento).html();
+    let htmlNovo = "";
+    if (novaSituacao == "N") {
+        htmlNovo = htmlAtual.replace("Entregou", "Não entregou").replace("fa-check", "fa-x");
+        $(elemento).removeClass("btn-success");
+        $(elemento).addClass("btn-danger");
+    } else if (novaSituacao == "I") {
+        htmlNovo = htmlAtual.replace("Não entregou", "Incompleta").replace("fa-x", "fa-triangle-exclamation");
+        $(elemento).removeClass("btn-danger");
+        $(elemento).addClass("btn-warning");
+    } else {
+        htmlNovo = htmlAtual.replace("Incompleta", "Entregou").replace("fa-triangle-exclamation", "fa-check");
+        $(elemento).removeClass("btn-warning");
+        $(elemento).addClass("btn-success");
+    }
+    $(elemento).html(htmlNovo);
 }
