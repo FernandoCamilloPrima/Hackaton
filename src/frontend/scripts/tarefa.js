831104196dad48d36883db773dc4b5fc5bd9ec5d
Fernando Camillo <fernando.camillo@prima.com.br>
2022-09-01 18:12:37 -0300

    Ajustes

--- src/frontend/scripts/tarefa.js
+++ src/frontend/scripts/tarefa.js
@@ -93,22 +93,31 @@ const obterIconeSituacao = function(situacao) {
 
 const carregarTela = function() {
     $("#dadosGerais").html(`
+        <h3 class="topo-texto">Aula ${dados.aula}, ${dados.disciplina.toHtmlEntities()}</h3>
+    `);
+    $("#dataAula").html(dados.dia);
+    //$("#horaAula").html(dados.hora);
+    $("#turma").html(dados.turma.toHtmlEntities());
+
+    /*
+    $("#dadosGerais").html(`
         <h2>${dados.periodo.toHtmlEntities()} - ${dados.disciplina.toHtmlEntities()} - ${dados.turma.toHtmlEntities()}</h2>
         <h3>Aula ${dados.aula} - ${dados.dia}</h3>
         <p><strong>Tarefa:</strong> ${dados.tarefaConteudo.toHtmlEntities()}</p>
         <p><strong>Data para entrega:</strong> ${dados.tarefaDataEntrega}</p>
     `);
+    */
 
     $.each(dados.alunos, function(i, aluno) {
         $('#listaAlunos').append(`
             <div class="card mb-3" style="background-color: #eeeeee">
                 <div class="card-body">
-                    <div class="row">
-                        <div class="col-sm-8">
+                    <div style="display: flex; align-items: center; justify-content: space-between;">
+                        <div>
                             ${aluno.nome.toHtmlEntities()}
                         </div>
-                        <div class="col-sm-4 text-end">
-                            <a data-codigo=${aluno.codigo} data-situacao="${aluno.situacao}" href="javascript:void(0)" onclick="alternar(this)" role="button" class="btn btn-sm btn-${obterClasseSituacao(aluno.situacao)} w-100 mt-1">
+                        <div class="text-end">
+                            <a data-codigo=${aluno.codigo} data-situacao="${aluno.situacao}" href="javascript:void(0)" onclick="alternar(this)" role="button" class="btn btn-sm btn-${obterClasseSituacao(aluno.situacao)} btn-situacao">
                                 <span class="${obterIconeSituacao(aluno.situacao)}" style="margin-right: 5px;"></span> ${obterDescricaoSituacao(aluno.situacao)} <span class="fa fa-caret-down" style="margin-left: 5px;"></span>
                             </a>
                         </div>
